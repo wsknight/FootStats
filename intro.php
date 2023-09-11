75ed1476c7dc375a7b1fd9da5599be20105b0c2a
Wsknight05 <457667-Wsknight05@users.noreply.replit.com>
2023-09-11 17:32:39 +0000

    Initial commit

---
+++ intro.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "FootStats";
+$dbname = "footballApp";
+
+//Create a Connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//check connection
+if(!$conn){
+  die("Connection failed: " .mysqli_connect_error() );
+}
+//SQL Statement
+$sql = "INSERT INTO games (gameNum, team1, team2, ODK)
+VALUES (1, 'team1string', 'team2string', 'ODK')";
+
+//SQL Query
+if(mysqli_query($conn, $sql)) {
+  echo "New Record Created";
+}else{
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+//Close Connection
+mysqli_close($conn);
+  
+?>
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,116 @@
+//Global Variables
+
+var slider = document.getElementById("slider");
+var sliderOutput = document.getElementById("valueNum");
+sliderOutput.innerHTML = calcSliderKick(slider.value);
+slider.oninput = function() {
+  sliderOutput.innerHTML = calcSliderKick(this.value);
+}
+
+
+getDate();
+//Functions
+function getDate() 
+{
+let date = new Date().toISOString().substring(0,10)
+document.getElementById("gameDate").value = date;
+}
+
+function changeScenarioText(str)
+{
+  var scenarioText = document.getElementById("scenarioText");
+  scenarioText.innerHTML = str;
+}
+
+function disableFields(name) //Function to make the fields of input readonly. name var is ID of Div 
+{
+  var fields = document.getElementById(name).getElementsByTagName('input');
+  
+}
+
+
+function calcSliderKick(num)
+{
+  let absnum = 50 - Math.abs(num);
+    if(num == 0)
+  {
+    return("On The: 50");
+  }
+  if(absnum == 0)
+  {
+    if(num == -50)
+    {
+      return("On The: Left 20 (Touchback)");
+    }
+    else
+    {
+      return("On The: Right 20 (Touchback)");
+    }
+  }
+    
+  if (num > 0) 
+  {
+    return("On The: Right " + absnum);
+  }
+  else
+  {
+    return("On The: Left " + absnum);
+  }
+}
+
+
+function calcSlider(num)
+{
+  let absnum = 50 - Math.abs(num);
+    if(num == 0)
+  {
+    return("On The: 50");
+  }
+  if(absnum == 0)
+  {
+    if(num == -50)
+    {
+      return("Touchdown!!!");
+    }
+    else
+    {
+      return("Touchdown!!!");
+    }
+  }
+    
+  if (num > 0) 
+  {
+    return("On The: Right " + absnum);
+  }
+  else
+  {
+    return("On The: Left " + absnum);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   cursor parking |||||||||||||
+//   ------------------------------
+//      M            |||||||||||||
+//      L 
