5732d0539795704c7d3a95415de048eb26f59a60
sppatel6 <143103398+sppatel6@users.noreply.github.com>
2023-09-20 16:26:31 +0000

    merged intro.php and second.php

--- intro.php
+++ intro.php
@@ -25,6 +25,36 @@ if(mysqli_query($conn, $sql)) {
 }else{
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
+$sql = "SELECT * FROM games"
+
+//SELECT example without *: firstName, lastName, email 
+//FROM mytable
+//WHERE lastName="Wheeler" AND firstName = "Wayne" <-- This selects every record that has the lastName variable equal wheeler, this is useful for specific condition record manipulation.
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result)>0){
+echo "<table>
+        <tr>
+            <th>gameNum</th>
+            <th>teamOne</th>
+            <th>teamTwo</th>
+        </tr>";
+    while($row = mysqli_fetch_assoc($result)){
+      echo "<tr>
+              <td>" . $row['$gameNum']. "</td>
+              <td>" . $row['$teamOne']. "</td>
+              <td>" . $row['$teamTwo']. "</td>
+            </tr>
+            ";
+    }
+echo "</table>";
+  
+}else{
+  die;
+}
+
 //Close Connection
 mysqli_close($conn);
 
