4a005ec71343b1e392ba7c4292b05cb3581b2088
wsknight <92493369+wsknight@users.noreply.github.com>
2023-09-20 17:06:47 +0000

    ewrew

--- intro.php
+++ intro.php
@@ -26,34 +26,7 @@ if(mysqli_query($conn, $sql)) {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-$sql2 = "SELECT * FROM games"
-
-//SELECT example without *: firstName, lastName, email 
-//FROM mytable
-//WHERE lastName="Wheeler" AND firstName = "Wayne" <-- This selects every record that has the lastName variable equal wheeler, this is useful for specific condition record manipulation.
-
-$result = mysqli_query($conn, $sql2);
-
-if(mysqli_num_rows($result)>0){
-echo "<table>
-        <tr>
-            <th>gameNum</th>
-            <th>teamOne</th>
-            <th>teamTwo</th>
-        </tr>";
-    while($row = mysqli_fetch_assoc($result)){
-      echo "<tr>
-              <td>" . $row['$gameNum']. "</td>
-              <td>" . $row['$teamOne']. "</td>
-              <td>" . $row['$teamTwo']. "</td>
-            </tr>
-            ";
-    }
-echo "</table>";
-  
-}else{
-  die;
-}
+header("Location: secondform.html");
 
 //Close Connection
 mysqli_close($conn);
