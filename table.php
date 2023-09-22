5987a4c78a6eb2b43be4c07ae295c7e6e0cb71f2
sppatel6 <143103398+sppatel6@users.noreply.github.com>
2023-09-22 15:17:04 +0000

    hehe

--- table.php
+++ table.php
@@ -24,40 +24,6 @@ if(!$conn){
 $sql = "INSERT INTO playInformation (playType, hash, backfieldCalls, oPlay, oStrength, playDirection)
 VALUES ('$playType' , '$hash' , '$backfieldCalls' , '$oPlay' , '$oStrength' , '$playDirection')";
 
-//sql grab statement
-$sql2 = "SELECT * FROM playInformation";
-
-$result = mysqli_query($conn, $sql2);
-
-if(mysqli_num_rows($result)>0){
-echo "<table>
-        <tr>
-            <th>Play Number</th>
-            <th>Run Or Pass</th>
-            <th>Hash</th>
-            <th>Backfield Calls</th>
-            <th>Offensive Play</th>
-            <th>Offensive Strength</th>
-            <th>Play Direction</th>
-        </tr>";
-    while($row = mysqli_fetch_assoc($result)){
-      echo "<tr>
-              <td>" . $row['playID'] . "</td>
-              <td>" . $row['playType']. "</td>
-              <td>" . $row['hash']. "</td>
-              <td>" . $row['backfieldCalls']. "</td>
-              <td>" . $row['oPlay']. "</td>
-              <td>" . $row['oStrength']. "</td>
-              <td>" . $row['playDirection']. "</td>
-            </tr>
-            ";
-    }
-echo "</table>";
-  
-}else{
-  echo "didn't work";
-}
-
 
 //Close Connection
 mysqli_close($conn);
