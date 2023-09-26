9638953a8a86d231cde040d11eece4701a3fbec4
wsknight <92493369+wsknight@users.noreply.github.com>
2023-09-26 17:33:48 +0000

    VAH

--- delete.php
+++ delete.php
@@ -1,23 +1,4 @@
-<html>
-  
-<head>  
-  
-<style type="text/css">
-th, td {
-  border: 1px solid;
-}
-table{
-  width: 100%;
-  border-collapse: collapse;
-}
-  
-</style>
-  
-</head>
-
-<body>
-
-<?php>
+<?php
 
 $servername = "localhost";
 $username = "root";
@@ -34,58 +15,14 @@ $playType = $_POST['playType'];
 //Create a Connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-//check connection
-if(!$conn){
-  die("Connection failed: " . mysqli_connect_error() );
+
+$id = $_GET['id'];
+$sql="DELETE FROM playInformation WHERE playID='$id'";
+
+$result=mysqli_query($conn,$sql);
+
+if($result)
+{
+  header("location:table.php");
 }
-
-//Seeing if it has received an id.
-if (isset($_GET['id']))
-  //{
- // echo $_GET['id'];
-  //$id=_GET['id'];
-  //echo "<div>
-   //     <p>Are you sure you want to delete : '$id'?<p>
-     //   <div>
-  //"
- // $delete=mysqli_query($conn,"DELETE FROM 'playInformation' WHERE 'playID'='$id'");
- // header("location:table.php");
- // die();
-//  }
-
-
-//SQL insert statement
-$sql = "INSERT INTO playInformation (playType, hash, backfieldCalls, oPlay, oStrength, playDirection)
-VALUES ('$playType' , '$hash' , '$backfieldCalls' , '$oPlay' , '$oStrength' , '$playDirection')";
-
-mysqli_query($conn, $sql);
-
-//sql grab statement
-$sqlSelect = "SELECT * FROM playInformation";
-
-$result = mysqli_query($conn, $sqlSelect);
-
-
-
-
- while($row = mysqli_fetch_assoc($result($_GET['id']))){
-      echo "<tr>
-              <td>" . $row['playID'] . "</td>
-              <td>" . $row['playType']. "</td>
-              <td>" . $row['hash']. "</td>
-              <td>" . $row['backfieldCalls']. "</td>
-              <td>" . $row['oPlay']. "</td>
-              <td>" . $row['oStrength']. "</td>
-              <td>" . $row['playDirection']. "</td>
-              <td> <a href='delete.php?id=".$row['playID']."' class='button'>Delete<a> <td>
-            </tr>
-            ";
-
-
 ?>
-
-</body>
-
-  
-  
-</html>
