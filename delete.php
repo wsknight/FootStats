6a299a10f8f17ded91659e1ac22c0913b8f0322b
wsknight <92493369+wsknight@users.noreply.github.com>
2023-09-26 17:52:07 +0000

    plz fix

--- delete.php
+++ delete.php
@@ -37,6 +37,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 $id = $_GET['id'];
 $sql="DELETE FROM playInformation WHERE playID='$id'";
+
 $result=mysqli_query($conn,$sql);
 
 if($result)
