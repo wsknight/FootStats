d38dd881174693cf09fb78128274503111a02b04
wsknight <92493369+wsknight@users.noreply.github.com>
2023-10-11 17:00:29 +0000

    'id'

--- edit.php
+++ edit.php
@@ -17,27 +17,27 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 $id = $_GET['id'];
 
-$query1 = "UPDATE playInformation SET backfieldCalls = '$backfieldCalls' WHERE playID = $id";
+$query1 = "UPDATE playInformation SET backfieldCalls = '$backfieldCalls' WHERE playID = '$id'";
 
 $result1 = mysqli_query($conn,$query1);
 
-$query2 = "UPDATE playInformation SET hash = '$hash' WHERE playID = $id";
+$query2 = "UPDATE playInformation SET hash = '$hash' WHERE playID = '$id'";
 
 $result2 = mysqli_query($conn,$query2);
 
-$query3 = "UPDATE playInformation SET oPlay = '$oPlay' WHERE playID = $id";
+$query3 = "UPDATE playInformation SET oPlay = '$oPlay' WHERE playID = '$id'";
 
 $result3 = mysqli_query($conn,$query3);
 
-$query4 = "UPDATE playInformation SET oStrength = '$oStrength' WHERE playID = $id";
+$query4 = "UPDATE playInformation SET oStrength = '$oStrength' WHERE playID = '$id'";
 
 $result4 = mysqli_query($conn,$query4);
 
-$query5 = "UPDATE playInformation SET playDirection = '$playDirection' WHERE playID = $id";
+$query5 = "UPDATE playInformation SET playDirection = '$playDirection' WHERE playID = '$id'";
 
 $result5 = mysqli_query($conn,$query5);
 
-$query6 = "UPDATE playInformation SET playType = '$playType' WHERE playID = $id";
+$query6 = "UPDATE playInformation SET playType = '$playType' WHERE playID = '$id'";
 
 $result6 = mysqli_query($conn,$query6);
 
