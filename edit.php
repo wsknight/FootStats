d2eadf178442f99ec35b83618787cd076b559cf7
wsknight <92493369+wsknight@users.noreply.github.com>
2023-10-11 17:06:45 +0000

    e

--- edit.php
+++ edit.php
@@ -21,6 +21,25 @@ $query1 = "UPDATE playInformation SET backfieldCalls = '$backfieldCalls' WHERE p
 
 $result1 = mysqli_query($conn,$query1);
 
+$query2 = "UPDATE playInformation SET hash = '$hash' WHERE playID = '$id'";
+
+$result2 = mysqli_query($conn,$query2);
+
+$query3 = "UPDATE playInformation SET oPlay = '$oPlay' WHERE playID = '$id'";
+
+$result3 = mysqli_query($conn,$query3);
+
+$query4 = "UPDATE playInformation SET oStrength = '$oStrength' WHERE playID = '$id'";
+
+$result4 = mysqli_query($conn,$query4);
+
+$query5 = "UPDATE playInformation SET playDirection = '$playDirection' WHERE playID = '$id'";
+
+$result5 = mysqli_query($conn,$query5);
+
+$query6 = "UPDATE playInformation SET playType = '$playType' WHERE playID = '$id'";
+
+$result6 = mysqli_query($conn,$query6);
 
 header("location:table.php");
 
