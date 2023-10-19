9dd9b1f9e78fece68d58e6724e41d8f1022ede9e
sppatel6 <143103398+sppatel6@users.noreply.github.com>
2023-10-19 20:21:32 +0000

    asdasdas

--- table.php
+++ table.php
@@ -18,8 +18,17 @@
 <?php
 
 include '/connection.php';
-  
-//sql grab statement
+
+$backfieldCalls = $_POST['backfieldCalls'];
+$hash = $_POST['hash'];
+$oPlay = $_POST['oPlay'];
+$oStrength = $_POST['oStrength'];
+$playDirection = $_POST['playDirection'];
+$playType = $_POST['playType'];
+
+$sql = "INSERT INTO playInformation (playType, hash, backfieldCalls, oPlay, oStrength, playDirection)
+VALUES ('$playType' , '$hash' , '$backfieldCalls' , '$oPlay' , '$oStrength' , '$playDirection')";
+
 $sqlSelect = "SELECT * FROM playInformation";
 
 $result = mysqli_query($conn, $sqlSelect);
