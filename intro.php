39f3a2a073c4a941a1a632aeded24ce9f2aeaed8
sppatel6 <143103398+sppatel6@users.noreply.github.com>
2023-10-19 21:03:37 +0000

    :3 math

--- intro.php
+++ intro.php
@@ -6,7 +6,22 @@ $gameNum = $_POST['gameNum'];
 $teamOne = $_POST['teamOne'];
 $teamTwo = $_POST['teamTwo'];
 $lineOfScrim = $_POST['lineOfScrimmage'];
-$lineToGain = $_POST['lineOfScrimmage'] + 10;
+$lineToGain = 0;
+
+if($lineOfScrim == 50){
+  $lineOfScrim = 20; 
+}
+if($lineOfScrim == -50){
+  $lineOfScrim = -20; 
+}
+if($lineOfScrim > 0){
+  $lineToGain = $lineOfScrim - 10;
+}else if($lineOfScrim == 0){
+  $lineToGain = 10;
+}else{
+  $lineToGain = $lineOfScrim + 10;
+}
+
 
 //SQL Statement
 $sql = "INSERT INTO games (gameNum, team1, team2, ODK)
