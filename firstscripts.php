73cec07ecc93abbf31b7ff91035298f880cf7667
wsknight <92493369+wsknight@users.noreply.github.com>
2023-10-23 17:11:02 +0000

    One BIG Form

--- firstscripts.php
+++ firstscripts.php
@@ -1,19 +1,39 @@
 <?php
 
 include 'connection.php';
-include 'varstorage.php';
+
+$gameNum = $_POST['gameNum'];
+$teamOne = $_POST['teamOne'];
+$teamTwo = $_POST['teamTwo'];
+$lineOfScrim = $_POST['lineOfScrimmage'];
+$lineToGain = 0;
+
+if($lineOfScrim == 50){
+  $lineOfScrim = 20; 
+}
+if($lineOfScrim == -50){
+  $lineOfScrim = -20; 
+}
+if($lineOfScrim > 0){
+  $lineToGain = $lineOfScrim - 10;
+}else if($lineOfScrim == 0){
+  $lineToGain = 10;
+}else{
+  $lineToGain = $lineOfScrim + 10;
+}
+
 
 //SQL Statement
 $sql = "INSERT INTO games (gameNum, team1, team2, ODK)
 VALUES ($gameNum, '$teamOne', '$teamTwo', 'K')";
 
-mysqli_query($conn, $sql);
+$firstinsert = mysqli_query($conn, $sql);
 
 //SQL Statement
 $sql2 = "INSERT INTO fieldInformation (gainOrLoss, lineOfScrim, lineToGain, down)
 VALUES (0, $lineOfScrim, $lineToGain, 1)";
 
-mysqli_query($conn, $sql2);
+$secondinsert = mysqli_query($conn, $sql2);
 
 $backfieldCalls = $_POST['backfieldCalls'];
 $hash = $_POST['hash'];
@@ -25,14 +45,14 @@ $playType = $_POST['playType'];
 $sql3 = "INSERT INTO playInformation (playType, hash, backfieldCalls, oPlay, oStrength, playDirection)
 VALUES ('$playType' , '$hash' , '$backfieldCalls' , '$oPlay' , '$oStrength' , '$playDirection')";
 
-$insert = mysqli_query($conn, $sql3);
+$thirdinsert = mysqli_query($conn, $sql3);
 
 $gainOrLoss = $_POST['yardline'];
 
 $sql4 = "INSERT INTO fieldInformation (gainOrLoss, lineOfScrim, lineToGain, down)
 VALUES ($gainOrLoss, 0, $lineToGain, 1)";
 
-$insert2 = mysqli_query($conn, $sql4);
+$fourthinsert2 = mysqli_query($conn, $sql4);
 
 header("Location: /table.php");
 
