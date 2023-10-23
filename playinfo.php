9945208ff5c1148379d0d8b0221fb843fc8e062a
sppatel6 <143103398+sppatel6@users.noreply.github.com>
2023-10-23 16:37:35 +0000

    😁

--- playinfo.php
+++ playinfo.php
@@ -1,6 +1,19 @@
 <?php
 
 include 'connection.php';
+include 'varstorage.php';
+
+//SQL Statement
+$sql = "INSERT INTO games (gameNum, team1, team2, ODK)
+VALUES ($gameNum, '$teamOne', '$teamTwo', 'K')";
+
+mysqli_query($conn, $sql);
+
+//SQL Statement
+$sql2 = "INSERT INTO fieldInformation (gainOrLoss, lineOfScrim, lineToGain, down)
+VALUES (0, $lineOfScrim, $lineToGain, 1)";
+
+mysqli_query($conn, $sql2);
 
 $backfieldCalls = $_POST['backfieldCalls'];
 $hash = $_POST['hash'];
@@ -9,18 +22,18 @@ $oStrength = $_POST['oStrength'];
 $playDirection = $_POST['playDirection'];
 $playType = $_POST['playType'];
 
-$sql = "INSERT INTO playInformation (playType, hash, backfieldCalls, oPlay, oStrength, playDirection)
+$sql3 = "INSERT INTO playInformation (playType, hash, backfieldCalls, oPlay, oStrength, playDirection)
 VALUES ('$playType' , '$hash' , '$backfieldCalls' , '$oPlay' , '$oStrength' , '$playDirection')";
 
-$insert = mysqli_query($conn, $sql);
+$insert = mysqli_query($conn, $sql3);
 
 $lineToGain = $_POST['lineToGain'];
 $gainOrLoss = $_POST['yardline'];
 
-$sql2 = "INSERT INTO fieldInformation (gainOrLoss, lineOfScrim, lineToGain, down)
+$sql4 = "INSERT INTO fieldInformation (gainOrLoss, lineOfScrim, lineToGain, down)
 VALUES ($gainOrLoss, 0, $lineToGain, 1)";
 
-$insert2 = mysqli_query($conn, $sql2);
+$insert2 = mysqli_query($conn, $sql4);
 
 header("Location: /table.php");
 
--- varstorage.php
+++ varstorage.php
@@ -22,19 +22,7 @@ if($lineOfScrim > 0){
   $lineToGain = $lineOfScrim + 10;
 }
 
-//SQL Statement
-$sql = "INSERT INTO games (gameNum, team1, team2, ODK)
-VALUES ($gameNum, '$teamOne', '$teamTwo', 'K')";
-
-mysqli_query($conn, $sql);
-
-//SQL Statement
-$sql2 = "INSERT INTO fieldInformation (gainOrLoss, lineOfScrim, lineToGain, down)
-VALUES (0, $lineOfScrim, $lineToGain, 1)";
-
-mysqli_query($conn, $sql2);
-
-header("Location: secondform.html?lineToGain = $lineToGain");
+header("Location: secondform.html");
 
 //Close Connection
 mysqli_close($conn);
